eec8d81e8d63cf470055f4bbc1044af68369c576
Daye Lee <leeda1801@gmail.com>
2021-10-27 16:53:14 +0900

    refactor: Remove useless router

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,16 +1,8 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import './App.css';
 import { Home } from './pages';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Router>
-  );
+  return <Home />;
 }
 
 export default App;
